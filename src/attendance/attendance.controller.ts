babe1fa173afb8f26694736770ebb7705c1a91bb
leonardotorricoIT <leonardo.torrico@assuresoft.tech>
2026-01-23 20:40:37 -0400

    paginacion

--- src/attendance/attendance.controller.ts
+++ src/attendance/attendance.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, Post, Get, Body, Param, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Body,
+  Param,
+  UseGuards,
+  Query,
+} from '@nestjs/common';
 import { AttendanceService } from './attendance.service';
 import { CreateAttendanceDto } from './dto/create-attendance.dto';
+import { FilterAttendanceDto } from './dto/filter-attendance.dto';
 import { TerminalTokenGuard } from 'src/guard/terminal-token.guard';
 
 @Controller('attendances')
 export class AttendanceController {
   constructor(private service: AttendanceService) {}
+
   @UseGuards(TerminalTokenGuard)
   @Post()
   create(@Body() dto: CreateAttendanceDto) {
@@ -13,12 +23,15 @@ export class AttendanceController {
   }
 
   @Get()
-  findAll() {
-    return this.service.findAll();
+  findAll(@Query() params: FilterAttendanceDto) {
+    return this.service.findAll(params);
   }
 
   @Get('branch/:branchId')
-  findByBranch(@Param('branchId') branchId: string) {
-    return this.service.findByBranch(branchId);
+  findByBranch(
+    @Param('branchId') branchId: string,
+    @Query() params: FilterAttendanceDto,
+  ) {
+    return this.service.findAll({ ...params, branchId });
   }
 }
