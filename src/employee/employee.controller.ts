4645c770ed57174c550a63260d5de8ac64773f2f
leonardotorricoIT <leonardo.torrico@assuresoft.tech>
2026-01-30 12:02:39 -0400

    feat: delete and update employees

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Post, Get, Param, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Param,
+  Body,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { EmployeeService } from './employee.service';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
+import { UpdateEmployeeDto } from './dto/update-employee.dto';
 
 @Controller('employees')
 export class EmployeeController {
@@ -14,5 +23,14 @@ export class EmployeeController {
   @Get()
   findAll() {
     return this.service.findAll();
+  }
+  @Patch(':carnet')
+  update(@Param('carnet') carnet: string, @Body() dto: UpdateEmployeeDto) {
+    return this.service.update(carnet, dto);
+  }
+
+  @Delete(':carnet')
+  remove(@Param('carnet') carnet: string) {
+    return this.service.remove(carnet);
   }
 }
